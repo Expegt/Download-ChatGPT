4cf3441129d4aa1b77be9c75b5572b99ed5e83a4
Expegt <128752249+Expegt@users.noreply.github.com>
2023-03-27 05:12:23 +0100

    Update script.js

--- JAVA workout/script.js
+++ JAVA workout/script.js
@@ -1,1 +1,19 @@
+/* Detect numeric odd and even */
 
+function oddOrEven(x) {
+  
+  if (!Number.isInteger(x) === true) {
+    return console.error("Input Invalid");
+  }
+  
+  if(x % 2 === 0) {
+    console.log("The number is even");
+  } else {
+      console.log("The number is odd");
+  }
+}
+  
+  oddOrEven (1)
+  oddOrEven (2)
+  oddOrEven (2)
+  oddOrEven (2)
